3b3ef523a623b7e72136755f624e6be19f5941fb
Your Name <you@example.com>
2020-04-27 15:21:07 +0800

    图片展示

---
+++ app/src/main/java/com/example/myapplication/View/ImageLoaderUtil.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.View;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import com.example.myapplication.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+/**
+ * Created by HMY
+ */
+public class ImageLoaderUtil {
+
+    public static ImageLoader getImageLoader(Context context) {
+        return ImageLoader.getInstance();
+    }
+
+    public static DisplayImageOptions getPhotoImageOption() {
+        Integer extra = 1;
+        DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true).cacheOnDisk(true)
+                .showImageForEmptyUri(R.drawable.ic_default_image).showImageOnFail(R.drawable.ic_default_image)
+                .showImageOnLoading(R.drawable.ic_default_image)
+                .extraForDownloader(extra)
+                .bitmapConfig(Bitmap.Config.RGB_565).build();
+        return options;
+    }
+
+    public static void displayImage(Context context, ImageView imageView, String url, DisplayImageOptions options) {
+        getImageLoader(context).displayImage(url, imageView, options);
+    }
+
+    public static void displayImage(Context context, ImageView imageView, String url, DisplayImageOptions options, ImageLoadingListener listener) {
+        getImageLoader(context).displayImage(url, imageView, options, listener);
+    }
+}
